3721ba2772aac60b973ad7f5ab251b2f43d1dbd2
Katherine Wong <katherinewongkw3@gmail.com>
2021-05-19 16:37:54 -0700

    install express generator

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, { userNewrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+
+const db = mongoose.connection; 
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+    console.log('Mongoose connected as ${db.host}:${db.port}');
+});
